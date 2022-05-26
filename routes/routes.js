e7bd73980aa47a09f4adb8ec2991d238a0e3f653
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-05-26 20:05:36 +0600

    fix: change in routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const auth = require("../middlewares/auth");
 
 //import controllers
 const userRegistration = require("../controllers/registration");
 const login = require("../controllers/login");
 const fetchData = require("../controllers/fetchData");
-const updateData = require("../controllers/updateData");
 const deleteData = require("../controllers/deleteData");
 const addData = require("../controllers/addData");
 //defining routes
@@ -24,15 +22,8 @@ router.post("/registration", userRegistration);
 //post api for user login
 router.post("/login", login);
 
-// router.post("/welcome", auth, (req, res) => {
-//   res.status(200).send("Welcome 🙌 ");
-// });
-
 // get api for users radio list
 router.get("/fetch", auth, fetchData);
-
-// update api for users
-router.put("/update", auth, updateData);
 
 //delete api for users
 router.post("/delete", auth, deleteData);
