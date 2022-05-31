7274011ed9c6a60015b6cbf141c6da4cbe0ec2b5
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-05-31 15:37:02 +0600

    edit: change in error handling route /fetch

--- controllers/fetchData.js
+++ controllers/fetchData.js
@@ -1,15 +1,15 @@
 const { User } = require("../models");
-const fetchData = async (req, res) => {
+const fetchData = async (req, res, next) => {
   try {
     const { email, token } = req.body;
     const existingUser = await User.find({ email: email, token: token });
     if (existingUser.length === 0) {
-      return res.json("user not found");
+      throw new Error("user not found");
     } else {
       return res.json(existingUser[0].radio);
     }
   } catch (err) {
-    res.json(err);
+    next(err);
   }
 };
 
