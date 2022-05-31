3e3737cb87e90186276eb425f212c45044cb5cec
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-05-31 14:57:31 +0600

    fix:jwt verify error handled asynchronously

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,15 +6,17 @@ const verifyToken = (req, res, next) => {
     req.body.token || req.query.token || req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    throw new Error("A token is required for authentication");
   }
-  try {
-    const decoded = jwt.verify(token, config.SECRET);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send("Invalid Token");
-  }
-  return next();
+
+  const verifiedUser = jwt.verify(token, config.SECRET, (err, decoded) => {
+    if (err) {
+      throw new Error("Invalid token");
+    } else {
+      return decoded;
+    }
+  });
+  next();
 };
 
 module.exports = verifyToken;
