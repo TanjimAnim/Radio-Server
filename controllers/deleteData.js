15bef03dfcbb512fc53c5ccd238620f81798a3a1
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-05-31 18:06:25 +0600

    edit: error handling added

--- controllers/deleteData.js
+++ controllers/deleteData.js
@@ -1,16 +1,16 @@
 const { User } = require("../models");
 
-const deleteData = async (req, res) => {
+const deleteData = async (req, res, next) => {
   try {
     const { email, token, radio } = req.body;
 
     if (radio === undefined || radio === "") {
-      return res.json("please enter a radio station");
+      throw new Error("please enter a radio station");
     }
     const existingUser = await User.find({ email: email, token: token });
 
     if (existingUser.length === 0) {
-      return res.json("user not found");
+      throw new Error("user not found");
     }
     const stationToDelete = existingUser[0].radio.findIndex(
       (element) => element === radio.toLowerCase()
@@ -21,13 +21,13 @@ const deleteData = async (req, res) => {
     existingUser[0].radio.splice(stationToDelete, 1);
     await existingUser[0].save((err, result) => {
       if (err) {
-        return res.json("error occured:", err);
+        throw new Error("error occured:", err);
       } else {
         return res.json("station has been succesfully removed");
       }
     });
   } catch (err) {
-    res.json(err);
+    next(err);
   }
 };
 
