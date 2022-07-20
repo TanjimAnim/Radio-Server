b6a29c3ef1e1178ca40873edb9099fa89af074c1
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-07-20 20:30:33 +0600

    feat:cors applied and changed port

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const routes = require("./routes/routes");
+const cors = require("cors");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "50mb" }));
 dotenv.config({ path: "./.env" });
 
+app.use(cors());
 app.use("/", routes);
 
 //connection to mongoDB
@@ -19,7 +21,7 @@ mongoose
   })
   .then(() => {
     console.log("Connected to database");
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3080);
   })
   .catch((error) => {
     console.log(error.message);
